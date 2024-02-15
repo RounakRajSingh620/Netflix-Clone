e48d64d8826a342534a4f5b61bbe3ea3b50d9875
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-02-16 00:18:28 +0530

    Added signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import styled from "styled-components";
+import BackgroundImage from '../components/BackgroundImage';
+import Header from '../components/Header';
 export default function Signup() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <Container>
+            <BackgroundImage />
+            <Header />
+            <div className="body flex column a-center j-center">
+                <div className="text flex column">
+                    <h1>Unlimited movies, TV shows and more</h1>
+                    <h4>Watch anywhere. Cancel anytime</h4>
+                    <h6>Ready to watch? Enter your email to create or restart membership</h6>
+                </div>
+                <div className="form">
+                    <input type="email" placeholder="Email Address" name="email" />
+                    <input type="password" placeholder="Password" name="password" />
+                    <button>Get Started</button>
+                </div>
+                <button>Log In</button>
+            </div>
+        </Container>
+    )
 }
+const Container = styled.div``;
