7f77ec3af5fe974fefc78b80e60003449de5d2c2
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-02-16 23:24:47 +0530

    Added BackgroundImage

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,21 +6,35 @@ export default function Signup() {
     return (
         <Container>
             <BackgroundImage />
-            <Header />
-            <div className="body flex column a-center j-center">
-                <div className="text flex column">
-                    <h1>Unlimited movies, TV shows and more</h1>
-                    <h4>Watch anywhere. Cancel anytime</h4>
-                    <h6>Ready to watch? Enter your email to create or restart membership</h6>
+            <div className="content">
+                <Header />
+                <div className="body flex column a-center j-center">
+                    <div className="text flex column">
+                        <h1>Unlimited movies, TV shows and more</h1>
+                        <h4>Watch anywhere. Cancel anytime</h4>
+                        <h6>Ready to watch? Enter your email to create or restart membership</h6>
+                    </div>
+                    <div className="form">
+                        <input type="email" placeholder="Email Address" name="email" />
+                        <input type="password" placeholder="Password" name="password" />
+                        <button>Get Started</button>
+                    </div>
+                    <button>Log In</button>
                 </div>
-                <div className="form">
-                    <input type="email" placeholder="Email Address" name="email" />
-                    <input type="password" placeholder="Password" name="password" />
-                    <button>Get Started</button>
-                </div>
-                <button>Log In</button>
             </div>
         </Container>
     )
 }
-const Container = styled.div``;
+const Container = styled.div`
+position:relative;
+.content{
+    position:absolute;
+    top:0;
+    left:0;
+    background-color:rgb(0,0,0,0.5);
+    height:100vh;
+    width:100vw;
+    display:grid;
+    grid-template-rows:15vh 85vh;
+}
+`;
