0c7bc562e09ac791549c52e35ac27193b075b2bf
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-02-26 23:33:26 +0530

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import logo from "../assets/logo.png"
+import { Link } from 'react-router-dom';
 export default function NavBar({ isScrolled }) {
     const links = [
         { name: "Home", link: "/" },
@@ -11,7 +13,21 @@ export default function NavBar({ isScrolled }) {
         <Container>
             <nav className={`flex ${isScrolled ? "scrolled" : " "}`}>
                 <div className="left flex a-center">
-                
+                    <div className="brand flex a-center j-center">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <ul className="links flex">
+                        {links.map((name, link) => {
+                            return (
+                                <li key={name}>
+                                    <Link to={link}>{name}</Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className="right flex a-center">
+                    <div className={`search`}></div>
                 </div>
             </nav>
 
