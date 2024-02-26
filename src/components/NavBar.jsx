4bcfe8604aebc6745de8f7fe6adb37e8932e1770
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-02-26 22:24:56 +0530

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+export default function NavBar({ isScrolled }) {
+    const links = [
+        { name: "Home", link: "/" },
+        { name: "TV Shows", link: "/tv" },
+        { name: "Movies", link: "/movies" },
+        { name: "My List", link: "/mylist" },
+    ];
+    return (
+        <Container>
+            <nav className={`flex ${isScrolled ? "scrolled" : " "}`}>
+                <div className="left flex a-center">
+                
+                </div>
+            </nav>
 
-export default function NavBar() {
-  return (
-    <div>
-      
-    </div>
-  )
+        </Container>
+    )
 }
+
+const Container = styled.div``;
