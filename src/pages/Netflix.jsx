c7c39d28f9c47e632461c84cfd06cb7020c45661
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-02-26 20:55:13 +0530

    Netflix

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavBar from '../components/NavBar';
 
 export default function Netflix() {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onScroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onScroll = null);
+  }
   return (
     <div>
-     
+      <NavBar isScrolled={isScrolled} />
     </div>
   )
 }
