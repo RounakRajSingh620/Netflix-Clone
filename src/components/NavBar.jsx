6a440dbc84534d953f420c320fa0a2ba6701022f
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-03-02 12:06:32 +0530

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -57,4 +57,69 @@ export default function NavBar({ isScrolled }) {
     )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+.scrolled{
+    background-color:black;
+}
+nav{
+    position:sticky;
+    top:0;
+    height:6.5rem;
+    width:100%;
+    justify-content:space-between;
+    position :fixed;
+    z-index:2;
+    padding:0 4rem;
+    align-items:center;
+    transition:0.3s ease-in-out;
+    .left{
+        gap:2rem;
+        .brand{
+            img{
+                height:4rem;
+            }
+        }
+        .links{
+            list-style-type:none;
+            gap:2rem;
+            li{
+                a{
+                    color:white;
+                    text-decoration:none;
+                }
+            }
+        }
+    }
+    .right{
+        gap:1rem;
+        button{
+            background-color:transparent;
+            border:none;
+            cursor:pointer;
+            &:focus{
+                outlins:none;
+                 
+            }
+            svg{
+                color:#f34242;
+                font-size:1.2rem;
+            }
+        }
+        .search{
+            display:flex;
+            gap:0.4rem;
+            align-items:center;
+            justify-content:center;
+            padding:0 2rem;
+            padding-left:0.5rem;
+            button{
+            background-color:transparent;
+            svg{
+                color:white;
+
+            }
+            }
+        }
+    }
+}
+`;
