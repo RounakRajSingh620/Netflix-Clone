1f2b395e05057674ace56d0f96dca77b9a74bd54
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-03-15 23:18:20 +0530

    Update Store

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import {
     createAsyncThunk,
     createSlice
 } from "@reduxjs/toolkit";
-import { API_KEY,TMBD_BASE_URL } from "../utils/constant";
+import { API_KEY, TMBD_BASE_URL } from "../utils/constant";
 import axios from "axios";
 
 const initialState = {
@@ -13,13 +13,27 @@ const initialState = {
 };
 
 export const getGenres = createAsyncThunk("netflix/genres", async () => {
-    const { data }
+    const {
+        data: { genres }, }
         = await axios.get(
             `${TMBD_BASE_URL}/genre/movie/list?api_key=${API_KEY}`
         );
-    console.log(data);
-    // return data;
+    return genres;
 });
+
+export const fetchMovies = createAsyncThunk(
+    "netflix/trending",
+    async ({ type }, thunkAPI) => {
+      const {
+        netflix: { genres },
+      } = thunkAPI.getState();
+      return getRawData(
+        `${TMDB_BASE_URL}/trending/${type}/week?api_key=${API_KEY}`,
+        genres,
+        true
+      );
+    }
+  );
 const NetflixSlice = createSlice({
     name: "Netflix",
     initialState,
